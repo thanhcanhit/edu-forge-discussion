a190226d0ba8c21011b902038e66c070558ccc1f
thanhcanhit <thanhcanh.dev@gmail.com>
2025-04-18 20:13:06 +0700

    Fix server listen address to bind to all interfaces

--- src/main.ts
+++ src/main.ts
@@ -34,7 +34,7 @@ async function bootstrap() {
       transform: true,
     }),
   );
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 
 void bootstrap().catch((err) => {
